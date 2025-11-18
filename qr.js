de0429812b71d01c123890b8ea20d0a65146b7bc
Rolly Maulana Awangga <awangga@gmail.com>
2025-11-18 09:42:00 +0700

    ganti endpooint

--- qr.js
+++ qr.js
@@ -1,7 +1,7 @@
 import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/whatsauth.js";
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
 
-wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
+wauthparam.auth_ws="d3NzOi8vYXBrLmZseS5kZXYvd3Mvd2hhdHNhdXRoL3B1YmxpYw==";
 //wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg3NzUyMDAwMzAwP3RleHQ9d2g0dDVhdXRoMA==";
 wauthparam.tokencookiehourslifetime=18;
